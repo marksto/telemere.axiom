7957910f6c16e50289e9338f87956a33cdcf30a8
Mark Sto <marksto@yourown.software>
2025-05-01 08:27:25 +0400

    Tune up the copyright notice wording and date

--- src/marksto/telemere/axiom.clj
+++ src/marksto/telemere/axiom.clj
@@ -1,10 +1,10 @@
-;; Copyright (c) Mark Sto, 2012. All rights reserved.  The use
-;; and distribution terms for this software are covered by the Eclipse
-;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;; which can be found in the file epl-v10.html at the root of this
-;; distribution.  By using this software in any fashion, you are
-;; agreeing to be bound by the terms of this license.  You must not
-;; remove this notice, or any other, from this software.
+;; Copyright (c) Mark Sto, 2025. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file `LICENSE` at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license.
+;; You must not remove this notice, or any other, from this software.
 
 (ns marksto.telemere.axiom
   "A set of things necessary for Telemere-to-Axiom integration.
